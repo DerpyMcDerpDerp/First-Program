0c93d008bbfef2d91478f05179861ffec4cffd8b
Silence <31050961+DerpyMcDerpDerp@users.noreply.github.com>
2018-09-13 12:25:28 -0400

    Add files via upload

---
+++ First Project/src/FirstClass.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @author Leo Makalsky
+ * First Program
+ *
+ */
+public class FirstClass 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.print("Gooooooooood Morning, Great Valley! " & "You're gonna die tonight");
+
+	}
+
+}
